b43968f54e5ce591df17e9b0567f11bc915d75d2
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2019-01-24 10:19:55 -0500

    assci perlin noise test

---
+++ hobby/perlin_space/js/main.js
@@ -0,0 +1,49 @@
+"use strict";
+const noiseIndexXSeed = 100213;
+const noiseIndexYSeed = 8132;
+const charSize = 18;
+
+function setup() {
+  noiseSeed(0);
+  // modcharsize
+  noiseDetail(8);
+  createCanvas(500, 800);
+  textSize(charSize);
+}
+
+function draw() {
+  background(255);
+  let noiseIndexX = noiseIndexXSeed + mouseX / width; //+ player.x
+  let noiseIndexY = noiseIndexYSeed + mouseY / height;
+  noStroke();
+
+  for (let i = 0; i < width / charSize; i++) {
+    for (let j = 0; j < height / charSize; j++) {
+      const x = i * charSize;
+      const y = j * charSize;
+
+      const yy = noise(i / charSize + noiseIndexX, j / charSize + noiseIndexY);
+      // fill(255*yy);
+      fill(0);
+      if ((yy > .2) && (yy < .3)) {
+        text(".", x, y);
+      }
+      if ((yy > .3) && (yy < .45)) {
+        text(",,", x, y);
+      }
+      if ((yy > .45) && (yy < .55)) {
+        text("o", x, y);
+      }
+      if ((yy > .55) && (yy < .65)) {
+        text("O", x, y);
+      }
+      if ((yy > .65) && (yy < .75)) {
+        text("m", x, y);
+      }
+      if ((yy > .75) && (yy < 1)) {
+        text("lll", x, y);
+      }
+      // ellipse(i*charSize,j*charSize,charSize);
+    }
+  }
+}
