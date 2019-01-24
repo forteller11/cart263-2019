4ab7f4a735ad96b1e4ad83e8a55bb95f66057075
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2019-01-24 17:39:59 -0500

    followalong week3B

--- followAlong/week3/js/main.js
+++ followAlong/week3/js/main.js
@@ -1,53 +1,67 @@
 "use strict";
+
 window.onload = main;
-let time;
-let spans = [];
-// spans = document.getElementsByClassName("redacted");
-
-function main() {
-  spans = document.getElementsByClassName("redacted");
-  for (let span of spans){
-    span.addEventListener('click',function(){
-      console.log("AHH");
-      if (span.className ==="highlighted"){
-        span.setAttribute("class","redacted");
-      }
-    });
+let secrets = [];
+let counter;
+let counterNumber = 0;
+function main(){
+  secrets = document.getElementsByClassName("secret");
+  counter = document.getElementById("counter");
+  counter.innerHTML = "you have found "+counterNumber+" secrets!";
+  for (let secret of secrets){
+    secret.addEventListener("mouseover",onHover);
   }
-  setInterval(update, 200);
+  // setInterval(update,20);
 }
-// update is pointer to function;
-// update() is value of function (all of its executed commands)
-// function update declares the value of the function
-function update() {
-  for (let span of spans){
-    if (Math.random(1) < .5){
-      // span.classList.add("hightlighted");
-      span.setAttribute("class","highlighted");
-    }
-    // if (Math.random(1) < .1){
-    //   span.classList.add("dog");
-    // }
+function onHover(e){
+  if (e.target.className === "secret"){
+    counterNumber ++;
+    counter.innerHTML = "you have found "+counterNumber+" secrets!";
+    e.target.className = "found";
   }
-  // console.log("Hey");
 }
 
-// function main(){
-//   let divs = document.getElementsByTagName('div');
-//   for (let d of divs){
-//     d.style.color = "red";
-//   }
-//   for (let d of divs){
-//     d.addEventListener('click',function(e){
-//       console.log(e);
-//       e.target.style.display = "none";
+
+
+// function update(){
+//   secrets = document.getElementsByClassName("secret");
+// }
+
+
+
+// "use strict";
+// window.onload = main;
+// let time;
+// let spans = [];
+// // spans = document.getElementsByClassName("redacted");
+//
+
+// function main() {
+//   spans = document.getElementsByClassName("redacted");
+//   for (let span of spans){
+//     span.addEventListener('click',function(){
+//       console.log("AHH");
+//       if (span.className ==="highlighted"){
+//         span.setAttribute("class","redacted");
+//       }
 //     });
 //   }
-//
-// // let $divs = $('div');
-// // $divs.fadeIn() = "none";
+//   setInterval(update, 200);
+// }
+// // update is pointer to function;
+// // update() is value of function (all of its executed commands)
+// // function update declares the value of the function
+// function update() {
+//   for (let span of spans){
+//     if (Math.random(1) < .5){
+//       // span.classList.add("hightlighted");
+//       span.setAttribute("class","highlighted");
+//     }
+//     // if (Math.random(1) < .1){
+//     //   span.classList.add("dog");
+//     // }
+//   }
+//   // console.log("Hey");
 // }
 //
-// function draw(){
-//
-// }
+// //
