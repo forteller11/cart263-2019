4500787e13e7e60ab795a6cc368c851d19a3e758
forteller11 <42951087+forteller11@users.noreply.github.com>
2019-01-28 14:57:43 -0500

    project1Tutorial got node.js and express installed correctly

---
+++ projects/project1PracticeNetworking/public/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function setup(){
+createCanvas(500,500);
+}
+
+function draw(){
+  background(51);
+  ellipse(mouseX,mouseY,30,30);
+}
--- projects/project1PracticeNetworking/server.js
+++ projects/project1PracticeNetworking/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
 var server = app.listen(3000);
+
+app.use(express.static('public'));
 console.log("socket server running");
 console.log("ahh");
