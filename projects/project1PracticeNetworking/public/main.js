1fdea778ba36804369e925de4fd078f1ba92ca50
forteller11 <42951087+forteller11@users.noreply.github.com>
2019-01-29 22:58:51 -0500

    project1 trying to instantiate all players stored by server on client connection
    currently I don't know how to tell the first connection where the server has no stored players

--- projects/project1PracticeNetworking/public/main.js
+++ projects/project1PracticeNetworking/public/main.js
@@ -8,64 +8,77 @@ function setup() {
   createCanvas(200, 200);
 }
 
-function draw(){
-    background(51);
-    for (let player of players){
-      player.draw();
-    }
+function draw() {
+  background(51);
+  for (let player of players) {
+    player.draw();
+  }
 }
 
-function main (){
+function main() {
   console.log("main");
   socket = io.connect('http://localhost:3000');
 
   socket.on('connect', function() { //on connection with server...
-    sessionID = socket.id; //save session id
-    console.log("sessionID:"+sessionID);
-    //fill up array with all current players,  also fill up screen with all current text
-    socket.players = players;
-    console.log(players);
-    let newPlayer = new Player(random(200),random(200),sessionID);
-    players.push(newPlayer);
-    socket.emit('newPlayer',newPlayer);
-
-  //   //if receive data that other player has moved
-  //   socket.on('playerMoved',function(data){ //find player ID which matches data, then set players pos to that data's pos
-  //     for (let i = 0; i < players.length; i ++){
-  //       if (data.id === players[i].id){
-  //         players[i].x = data.x;
-  //         players[i].y = data.y;
-  //       }
-  //     }
-  //   });
-  // });
-  //
-  socket.on('playerMoved', function(data){
-    for (let i = 0; i < players.length; i ++){ //move current player (for immediate response)
-      if (data.id === players[i].id){
-        players[i].x = data.x;
-        players[i].y = data.y;
-        break; //break out of loop
-      }
-      console.log("received player movements");
-    }
-  });
-  document.addEventListener("mousemove",function (e){
-    console.log("mouseDownFunction");
-    for (let i = 0; i < players.length; i ++){ //move current player (for immediate response)
-      if (sessionID === players[i].id){
-        players[i].x = e.clientX;
-        players[i].y = e.clientY;
-        let data = {
-          x: e.clientX,
-          y: e.clientY,
-          id: players[i].id
+    socket.on('initPlayers', function(initPlayersData) { //before doing anything, wait for server to tell client where all existing players are
+      console.log('initPlayersData');
+      console.log(initPlayersData);
+      console.log(initPlayersData.legnth);
+      if (initPlayersData.length > 0){
+        for (let i = 0; i < initPlayersData.length; i ++){ //using initPlayesData create fully fledged player data with methods and all
+          players.push(new Player(initPlayersData[i].x,initPlayersData[i].y,initPlayersData[i].id));
+          console.log("pushPlayer!");
         }
-        socket.emit('playerMoved',data);
-        break; //break out of loop
+        console.log("players post initialization");
+        console.log(players);
+      } else {
+        console.log("there are no players ")
       }
-    }
+      sessionID = socket.id; //save session id
+      console.log("sessionID:" + sessionID);
+      //fill up array with all current players,  also fill up screen with all current text
+      let newPlayer = new Player(random(200), random(200), sessionID);
+      players.push(newPlayer);
+      socket.emit('newPlayer', newPlayer);
 
+      //   //if receive data that other player has moved
+      //   socket.on('playerMoved',function(data){ //find player ID which matches data, then set players pos to that data's pos
+      //     for (let i = 0; i < players.length; i ++){
+      //       if (data.id === players[i].id){
+      //         players[i].x = data.x;
+      //         players[i].y = data.y;
+      //       }
+      //     }
+      //   });
+      // });
+      //
+      socket.on('playerMoved', function(data) {
+        for (let i = 0; i < players.length; i++) { //move current player (for immediate response)
+          if (data.id === players[i].id) {
+            players[i].x = data.x;
+            players[i].y = data.y;
+            break; //break out of loop
+          }
+          console.log("received player movements");
+        }
+      });
+      document.addEventListener("mousemove", function(e) {
+        console.log("mouseDownFunction");
+        for (let i = 0; i < players.length; i++) { //move current player (for immediate response)
+          if (sessionID === players[i].id) {
+            players[i].x = e.clientX;
+            players[i].y = e.clientY;
+            let data = {
+              x: e.clientX,
+              y: e.clientY,
+              id: players[i].id
+            }
+            socket.emit('playerMoved', data);
+            break; //break out of loop
+          }
+        }
+
+      });
+    });
   });
-});
 }
--- projects/project1PracticeNetworking/server.js
+++ projects/project1PracticeNetworking/server.js
@@ -2,17 +2,26 @@ let express = require('express');
 let app = express();
 let server = app.listen(3000);
 let players = [];
+// let players = [let player={
+//   x: 100,
+//   y: 100,
+//   id: "23fdsfds90fds"
+// }];
 
 app.use(express.static('public'));
 console.log("socket server running");
 
 let socket = require('socket.io');
-socket.players = players; //add property to socket object that is array of player objects
 let io = socket(server);
+
 
 //on new connection, call anynomous function with socket as event
 io.sockets.on('connection',function(socket){
-  console.log(socket.players);
+  console.log("server side playerobj array")
+    console.log(players);
+  socket.emit('initPlayers',players); //immediately after connection tell client the state of all player objs
+
+
   console.log('new connection:' + socket.id);
 
   socket.on('playerMoved',function(data){ //when a msg is received by client called mouseMove
