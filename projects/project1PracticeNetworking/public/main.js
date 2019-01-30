7e8dfbadad0a42edfa0931ef0bc8bb6b9fecb74a
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2019-01-30 11:38:32 -0500

    project1 tut some bug fixing and cleaning of code
    need to figure out why only client which emits can receive data

--- projects/project1PracticeNetworking/public/main.js
+++ projects/project1PracticeNetworking/public/main.js
@@ -23,15 +23,15 @@ function main() {
     socket.on('initPlayers', function(initPlayersData) { //before doing anything, wait for server to tell client where all existing players are
       console.log('initPlayersData');
       console.log(initPlayersData.length);
-      if (!(initPlayersData.length === 0)){
-        for (let i = 0; i < initPlayersData.length; i ++){ //using initPlayesData create fully fledged player data with methods and all
-          players.push(new Player(initPlayersData[i].x,initPlayersData[i].y,initPlayersData[i].id));
+      if (!(initPlayersData.length === 0)) {
+        for (let i = 0; i < initPlayersData.length; i++) { //using initPlayesData create fully fledged player data with methods and all
+          players.push(new Player(initPlayersData[i].x, initPlayersData[i].y, initPlayersData[i].id));
           console.log("pushPlayer!");
         }
         console.log("players post initialization");
         console.log(players);
       } else {
-        console.log("there are no players ")
+        console.log("there are no players ");
       }
 
       sessionID = socket.id; //save session id
@@ -41,29 +41,15 @@ function main() {
       players.push(newPlayer);
       socket.emit('newPlayer', newPlayer);
 
-
-      //if receive data that other player has moved
-      socket.on('playerMoved', function(data) {
-        console.log("received player movements");
-        for (let i = 0; i < players.length; i++) { //move current player (for immediate response)
-          if (data.id === players[i].id) {
-            players[i].x = data.x;
-            players[i].y = data.y;
-            break; //break out of loop
-          }
-
-        }
-      });
-
-      document.addEventListener("mousemove", function(e) {
+      document.addEventListener('mousemove', function(e) {
         console.log("mouseDownFunction");
         for (let i = 0; i < players.length; i++) { //move current player (for immediate response)
           if (sessionID === players[i].id) {
             players[i].x = e.clientX;
             players[i].y = e.clientY;
             let data = {
-              x:  players[i].x,
-              y:  players[i].y,
+              x: players[i].x,
+              y: players[i].y,
               id: players[i].id
             }
             socket.emit('playerMoved', data);
@@ -73,12 +59,34 @@ function main() {
 
       });
 
+
       //if receive data that other player has moved
-      socket.on('newPlayer', function(data) {
-        console.log("NEW PLAYER JOINED FROM OTHER SESSION");
-        players.push(new Player(data.x,data.y,data.id));
+      socket.on('playerMoved', function(data) {
+        console.log("dataID");
+        console.log(data.id);
+        console.log("sessionID");
+        console.log(sessionID);
+        if (!(data.id === sessionID)) { //if not reffering to self
+          console.log("received player movements");
+          for (let i = 0; i < players.length; i++) { //move current player (for immediate response)
+            if (data.id === players[i].id) {
+              console.log("found appropriate player");
+              players[i].x = data.x;
+              players[i].y = data.y;
+              break; //break out of loop
+            }
+
+          }
+        }
       });
 
+      //if receive data that other player has moved
+      socket.on('newPlayer', function(data) {
+        if (!(data.id === sessionID)) { //if not reffering to self
+          console.log("NEW PLAYER JOINED FROM OTHER SESSION");
+          players.push(new Player(data.x, data.y, data.id));
+        }
+      });
     });
   });
 }
