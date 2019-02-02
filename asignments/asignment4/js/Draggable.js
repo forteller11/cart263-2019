3cb826d78100b11e5d19a2ec196bbcd8ea6be59a
forteller11 <42951087+forteller11@users.noreply.github.com>
2019-02-02 14:06:31 -0500

    asignment4 simplifying dragging code and fixing bugs

--- asignments/asignment4/js/Draggable.js
+++ asignments/asignment4/js/Draggable.js
@@ -1,80 +1,89 @@
 "use strict";
-class Draggable extends Image{
-  constructor(imageUrl,x=randomRange(0,window.innerWidth),y=randomRange(0,window.innerHeight),width=200,height=200){
-    super(imageUrl,x,y,width,height);
+class Draggable extends Image {
+  constructor(imageUrl, x = randomRange(0, window.innerWidth), y = randomRange(0, window.innerHeight), width = 200, height = 200) {
+    super(imageUrl, x, y, width, height);
     this.cooked = 1;
     this.hueMult = 90;
     this.dragging = false;
     this.deltaMouseX = null;
     this.deltaMouseY = null;
-    for (let element of this.elements){
-        element.style.display = "none";
-        element.draggable = false;
-        // element.filter = "hue-rotate(100deg)";
+    for (let element of this.elements) {
+      element.style.display = "none";
+      element.draggable = false;
     }
     this.elements[0].style.display = "block";
+    this.updateStyle();
 
-    this.container.addEventListener('mouseover',(e)=>{
+    this.container.addEventListener('mouseover', (e) => {
       console.log('hover');
-      if (this.dragging === false){ //dont overide dragg image
-        for (let element of this.elements){
-            element.style.display = "none";
+      if (this.dragging === false) { //dont overide dragg image
+        for (let element of this.elements) {
+          element.style.display = "none";
         }
         this.elements[1].style.display = "block";
       }
     });
 
-    this.container.addEventListener('mouseleave',(e)=>{ //mouse off
-      console.log('mouseout');
-      for (let element of this.elements){
+    this.container.addEventListener('mouseleave', (e) => { //mouse off
+      if (this.dragging === false) {
+        console.log('mouseleave');
+        for (let element of this.elements) {
           element.style.display = "none";
+        }
+        this.elements[0].style.display = "block";
+      }
+    });
+
+    document.addEventListener('mouseleave', (e) => { //mouse off
+      console.log('windowmouseout');
+      this.dragging = false;
+      for (let element of this.elements) {
+        element.style.display = "none";
       }
       this.elements[0].style.display = "block";
     });
 
-    this.container.addEventListener('mousedown',(e)=>{
+    this.container.addEventListener('mousedown', (e) => {
       console.log('PRESS');
-      if ((e.clientX < this.x+this.width)&&(e.clientX > this.x)){ //if mouse on image
-        if ((e.clientY < this.y+this.height)&&(e.clientY > this.y)){
-          this.currentUrl = this.pickedUpHoverUrl;
-          for (let element of this.elements){
-              element.style.display = "none";
-          }
-          this.elements[2].style.display = "block";
 
-          this.dragging = true; //start dragging
-          this.deltaMouseX = this.x - e.clientX;
-          this.deltaMouseY = this.y - e.clientY;
-        }
+      for (let element of this.elements) {
+        element.style.display = "none";
       }
+      this.elements[2].style.display = "block";
+
+      this.dragging = true; //start dragging
+      this.deltaMouseX = this.x - e.clientX;
+      this.deltaMouseY = this.y - e.clientY;
+
+
     });
 
-    this.container.addEventListener('mouseup',()=>{
+    document.addEventListener('mouseup', () => {
       console.log("RELEASE");
       this.dragging = false;
-      for (let element of this.elements){
-          element.style.display = "none";
+      for (let element of this.elements) {
+        element.style.display = "none";
       }
       this.elements[1].style.display = "block";
     });
 
-    this.container.addEventListener('mousemove',(e)=>{
+    this.container.addEventListener('mousemove', (e) => {
       this.drag(e);
     });
   }
 
-  drag(e){
-    if (this.dragging === true){
+  drag(e) {
+    if (this.dragging === true) {
       this.x = e.clientX + this.deltaMouseX;
       this.y = e.clientY + this.deltaMouseY;
     }
     this.updateStyle();
   }
 
-  updateStyle(){
+  updateStyle() {
     super.updateStyle();
-    for (let element of this.elements){
-        element.style.filter = "hue-rotate("+this.hueMult*this.cooked+"deg)";
+    for (let element of this.elements) {
+      element.style.filter = "hue-rotate(" + this.hueMult * this.cooked + "deg)";
     }
   }
 }
