5161ddec1a975070dfe8b6cfe0f0bf8026323236
Charly Yan Miller <fortelgaming@gmail.com>
2019-02-14 22:51:33 -0500

    hobby

--- hobby/zoom/js/main.js
+++ hobby/zoom/js/main.js
@@ -3,19 +3,24 @@
 window.onload = main;
 
 let videoSize = 100;
-let frameWidth = videoSize*16;
-let frameHeight = videoSize*9;
+let frameWidth = window.innerWidth * .7;;
+let frameHeight = frameWidth * 9 / 16;
 let zoomToHorz = .5; //zoom into horz center of screen = .5
 let zoomToVert = .5; // 0 = top, .5, center, 1 bottom
-let centerXOffset = (window.innerWidth - frameWidth)/2;
-let centerYOffset = (window.innerHeight - frameHeight)/2;
+let centerXOffset = (window.innerWidth - frameWidth) / 2;
+let centerYOffset = (window.innerHeight - frameHeight) / 2;
 let x = 0;
 let y = 0;
 let video;
 let container;
 
-let zoomAmount = 1;
+let zoomTarget = 1;
+let zoomCurrent = 1;
+const zoomChangePerFrame = .1;
 const zoomIncreaseAmount = .25;
+
+let leftKeysHeld = 0;
+let rightKeysHeld = 0;
 
 function main() {
   video = document.getElementById('video');
@@ -23,84 +28,102 @@ function main() {
 
   container = document.getElementById('container');
   container.style.position = "absolute";
-  container.style.width = frameWidth+"px";
+  container.style.width = frameWidth + "px";
   container.style.height = frameHeight + 'px';
   // frameWidth = video.width;
   // frameHeight = video.height;
 
   document.addEventListener('keydown', (e) => {
-    calculateZoomAmount(e,1);
+    calculateZoomAmount(e, 1);
   });
   document.addEventListener('keyup', (e) => {
-    calculateZoomAmount(e,-1);
+    calculateZoomAmount(e, -1);
   });
 
   console.log(video);
 
-  setInterval(update,16.7);
+  setInterval(update, 16.7);
 }
 
 function update() {
+  if ((leftKeysHeld > 0) && (rightKeysHeld > 0)) { //start video if both hands on at least one key
+    video.play();
+  } else {
+    video.pause();
+  }
+
+  //animate smoothly between zoom levels
+  const desiredZoomChange = zoomTarget - zoomCurrent;
+  const zoomChange = desiredZoomChange * zoomChangePerFrame;
+  zoomCurrent += zoomChange;
+
   // console.log("udpate");a
   container.style.left = centerXOffset + 'px';
   container.style.top = centerYOffset + 'px';
 
-  video.width = frameWidth*zoomAmount;
-  video.height = frameHeight*zoomAmount;
-  video.style.left = -(zoomAmount-1)*frameWidth*zoomToHorz  + 'px';
-  video.style.top = -(zoomAmount-1)*frameHeight*zoomToVert  + 'px';
+  video.width = frameWidth * zoomCurrent;
+  video.height = frameHeight * zoomCurrent;
+  video.style.left = -(zoomCurrent - 1) * frameWidth * zoomToHorz + 'px';
+  video.style.top = -(zoomCurrent - 1) * frameHeight * zoomToVert + 'px';
 }
 
 
-function calculateZoomAmount(e,sign) {
+function calculateZoomAmount(e, sign) {
   video.play();
-  if (e.repeat === false){
-  console.log(e.keyCode );
-  switch (e.keyCode) {
-    case 65: //a
-      zoomAmount += zoomIncreaseAmount*sign;
-      break;
+  if (e.repeat === false) {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+      case 65: //a
+        zoomTarget += zoomIncreaseAmount * sign;
+        leftKeysHeld += 1 * sign;
+        break;
 
       case 97: //a
-        zoomAmount += zoomIncreaseAmount*sign;
+        zoomTarget += zoomIncreaseAmount * sign;
+        leftKeysHeld += 1 * sign;
         break;
 
-    case 83: //s
-      zoomAmount += zoomIncreaseAmount*sign;
-      break;
-
-      case 115: //s
-        zoomAmount += zoomIncreaseAmount*sign;
+      case 83: //s
+        zoomTarget += zoomIncreaseAmount * sign;
+        leftKeysHeld += 1 * sign;
         break;
 
-    case 68: //d
-      zoomAmount += zoomIncreaseAmount*sign;
-      break;
 
-      case 100: //d
-        zoomAmount += zoomIncreaseAmount*sign;
+      case 68: //d
+        zoomTarget += zoomIncreaseAmount * sign;
+        leftKeysHeld += 1 * sign;
         break;
 
-    case 74: //j
-      zoomAmount += zoomIncreaseAmount*sign;
-      break;
+      case 70: //f
+        zoomTarget += zoomIncreaseAmount * sign;
+        leftKeysHeld += 1 * sign;
+        break;
+      case 71: //g
+        zoomTarget += zoomIncreaseAmount * sign;
+        leftKeysHeld += 1 * sign;
+        break;
 
+      case 72: //h
+        zoomTarget += zoomIncreaseAmount * sign;
+        rightKeysHeld += 1 * sign;
+        break;
+      case 74: //j
+        zoomTarget += zoomIncreaseAmount * sign;
+        rightKeysHeld += 1 * sign;
+        break;
 
+      case 75: //k
+        zoomTarget += zoomIncreaseAmount * sign;
+        rightKeysHeld += 1 * sign;
+        break;
 
-    case 75: //k
-      zoomAmount += zoomIncreaseAmount*sign;
-      break;
+      case 76: //L
+        zoomTarget += zoomIncreaseAmount * sign;
+        rightKeysHeld += 1 * sign;
+        break;
 
-
-
-    case 76: //L
-      zoomAmount += zoomIncreaseAmount*sign;
-      break;
-
-
-
-    default:
-      break;
+      default:
+        break;
+    }
   }
-}
 }
