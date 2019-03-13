dfbaa596ada06281caa65d18a8da85b10f1d9d54
Charly Yan Miller <fortelgaming@gmail.com>
2019-03-13 01:41:28 -0400

    project2 couldn't get youtube api working,
    instead I copy pasted the video id's of 100 youtube videos from the youtube trending page (on an incognito browser with no cookies or history) to get a sort of lowest common dominator recommded videos which I will use in my project

--- projects/project2/js/main.js
+++ projects/project2/js/main.js
@@ -1,37 +1,79 @@
 'use strict';
 
-// window.onload = main;
-// window.myJsonCallback = function(data){
-//   console.log(data);
-//   console.log('ahh');
+window.onload = main;
+
+function main(){
+  console.log('main');
+  let url = "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?
+        part=snippet
+        &order=viewCount
+        &q=skateboarding+dog
+        &type=video
+        &videoDefinition=high";
+
+        console.log(url);
+}
+// function setup(){
+//   noCanvas();
+//
+//   // let url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=rainbow"
+//   let url = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch?v=nZ5dRiSlZRE&callback=function(){console.log("hey")}';
+//   loadJSON(url, gotData,'jsonp');
+//
+//   function gotData(data){
+//     console.log(data);
+//   }
 // }
 
-function setup(){
-  noCanvas();
-  let url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=rainbow"
-  // let url = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=myJsonCallback';
-  loadJSON(url, (data) => {
-      console.log(data);
-  }, 'jsonp');
-}
-
+//parse yourself, go until you find ... then return that value
 // function main() {
 //
-//   // let url = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=myJsonCallback';
-//   // loadJSON(url, (data) => {
-//   //     console.log(data);
-//   // });
+//   let url = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=myJsonCallback';
+//
 //   // let script = document.createElement('script');
 //   // script.src = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=myJsonCallback'
 //   // script.type = "application/json"
 //   // let promise = fetch(script.src);
 //   // promise.then
 //   // document.body.appendChild(script)
-//
-//
+//   //
+//   //
 //   // console.log('hello');
 //   // body = document.getElementById('body');
-//   // let promise = fetch('http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=my_embed_function');
-//   // promise.then(console.log(promise));
 //
+//   let promise = fetch('http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=my_embed_function');
+//   promise.then(console.log(promise));
+//
+// }
+
+// function getData(data)
+// {
+//     console.log('IGOTITTTTTTTTTTTTTTTTTTTTTTTTTTTT');
+// }
+// function main(){
+// let url = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch?v=nZ5dRiSlZRE&format=json&callback=getData'
+// url =  "https://www.youtube.com/embed/ZK3O402wf1c"
+// let script = document.createElement('script');
+// script.src = url;
+//
+// // window.open(script.src);
+//
+// document.getElementsByTagName('head')[0].appendChild(script);
+// }
+// // or document.head.appendChild(script) in modern browsers
+
+// function main(){
+//
+//   console.log(document.body);
+//   let url = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch?v=nZ5dRiSlZRE&callback='+getData;
+//   // url = 'https://www.youtube.com/watch?v=2vOPEuiGXVo';
+//     window.open(url);
+//   let promise = fetch(url);
+//   promise.then(response => console.log(response));
+//
+// }
+//
+// function getData (getData){
+//   console.log(document.body);
+//   return document.body;
 // }
