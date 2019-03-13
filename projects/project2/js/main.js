9ca764a6bebf7760aa811aa5d1f243291a34f0f3
Charly Yan Miller <fortelgaming@gmail.com>
2019-03-12 21:19:59 -0400

    project2 resorted to using p5's cross domain ajax request wrapper function loadJSON(x,y,'jsonp')
    tried doing things in native js but I don't understand what's happening enough yet so I am just resorting to p5's high level abstraction at the moment, perhaps I will come back to this later in pure js as the code itself is still pretty concise

--- projects/project2/js/main.js
+++ projects/project2/js/main.js
@@ -1,8 +1,37 @@
 'use strict';
 
-window.onload = main;
-let body;
-function main(){
-  console.log('hello');
-  body = document.getElementById('body');
+// window.onload = main;
+// window.myJsonCallback = function(data){
+//   console.log(data);
+//   console.log('ahh');
+// }
+
+function setup(){
+  noCanvas();
+  let url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=rainbow"
+  // let url = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=myJsonCallback';
+  loadJSON(url, (data) => {
+      console.log(data);
+  }, 'jsonp');
 }
+
+// function main() {
+//
+//   // let url = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=myJsonCallback';
+//   // loadJSON(url, (data) => {
+//   //     console.log(data);
+//   // });
+//   // let script = document.createElement('script');
+//   // script.src = 'http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=myJsonCallback'
+//   // script.type = "application/json"
+//   // let promise = fetch(script.src);
+//   // promise.then
+//   // document.body.appendChild(script)
+//
+//
+//   // console.log('hello');
+//   // body = document.getElementById('body');
+//   // let promise = fetch('http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=my_embed_function');
+//   // promise.then(console.log(promise));
+//
+// }
