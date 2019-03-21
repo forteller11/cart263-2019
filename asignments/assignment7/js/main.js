e935e38e5de8302e1ee78ddf4f59ed38dfdc4820
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2019-03-21 17:05:06 -0400

    assignment7 visualized sounds

--- asignments/assignment7/js/main.js
+++ asignments/assignment7/js/main.js
@@ -33,14 +33,27 @@ let synth;
 
 let beatIndex = 0;
 let beatLength = 300/4;
-let beatNumber = 4*4 * 4;
+let beatNumber = 2*4 * 4;
 let beats = []; //4*4 beats
 let mouseDown = false;
+let canvas;
+let canvasCtx;
 for (let i = 0; i < beatNumber; i ++){ //create 2D array
   beats[i] = []; //will be arr of howler sound objs
 }
 
 function main(){
+
+  console.log('main');
+  console.log('main');
+  canvas = document.createElement('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  document.body.appendChild(canvas);
+  canvasCtx = canvas.getContext('2d');
+  // canvasCtx.style.zIndex = 100;
+  console.log(canvasCtx);
+
 console.log(beats)
   hihat = new Pizzicato.Sound('assets/hihat.wav');
 
@@ -67,6 +80,10 @@ setTimeout((playSoundsOfBeat),beatLength);
 
 }
 function playSoundsOfBeat(){
+  let canvasColor = 0;
+  canvasCtx.fillStyle =`rgba(${canvasColor},${canvasColor},${canvasColor},${.2})`;
+  canvasCtx.fillRect(0,  0, window.innerWidth, window.innerHeight);
+
   if (mouseDown === true){ //if mouse is down
   console.log(beats);
   let newSynth = new Pizzicato.Sound({
@@ -75,6 +92,8 @@ function playSoundsOfBeat(){
         frequency: window.innerHeight-mouseY
     }
 });
+newSynth.x = mouseX;
+newSynth.y = mouseY;
 newSynth.volume = mapFromRanges(mouseX,0,window.innerWidth,0,1);
   beats[beatIndex].push(newSynth);
 }
@@ -89,6 +108,12 @@ newSynth.volume = mapFromRanges(mouseX,0,window.innerWidth,0,1);
 // console.log(`previousBeat:${previousBeat} currentBeat:${beatIndex}`);
   for (let i = 0; i < beats[beatIndex].length; i ++) {
      beats[beatIndex][i].play();
+     let minSize = 2;
+     let maxSize = 100;
+     console.log(beats[beatIndex][i].volume);
+     let size = beats[beatIndex][i].volume*maxSize;
+       canvasCtx.fillStyle=`rgb(${ran(255)},${ran(255)},${ran(255)})`;
+       canvasCtx.fillRect( beats[beatIndex][i].x-size/2,  beats[beatIndex][i].y-size/2, size, size);
      // beats[beatIndex][i].pause();
   }
 // console.log
@@ -96,4 +121,5 @@ newSynth.volume = mapFromRanges(mouseX,0,window.innerWidth,0,1);
   if (beatIndex >= beatNumber) { beatIndex = 0}; //reset beat
   document.getElementById('beatCounter').innerHTML = `beat:${beatIndex}`;
   setTimeout(playSoundsOfBeat,beatLength);
+
 }
