c68c0c489f0a78448a8336de61f84cb96a7e243d
Charly Yan Miller <fortelgaming@gmail.com>
2019-03-23 23:53:57 -0400

    proj3 now parser stores individual faces as seperate arrays in a larger (2D) arrays of all faces

--- projects/project3/js/main3.js
+++ projects/project3/js/main3.js
@@ -43,41 +43,67 @@ function main() {
 function convertObjToVtData(obj) {
   let vArr = []; //vertices
   let vnArr = []; //vertexNormals
-  let fArr = []; //face
+  let fArr = []; //array of arrays of connected vertex's
+  let faceIndex = 0;
+  fArr[faceIndex] = []; //prep 2D array
   let currentWord = ''; //string of current num
   let currentDataType = 'irrelevant';
   for (let i = 0; i < obj.length; i++) {
     // console.log(obj[i]);
     if ((obj[i] === ' ') || (obj[i] === '\n') || obj[i] === '/') { //if at end of word, push data to relevant data type if currentDataType is a keyword
-      if (!(Number.isNaN(Number(currentWord)))){ //if string is numeric, then push it to approrpaite array depending of currentDataType
-        if (currentDataType === 'irrelevant')  {}
-        if (currentDataType === 'vertex')      {vArr.push(Number(currentWord)); console.log(currentWord)}
-        if (currentDataType === 'vertexNormal'){vnArr.push(Number(currentWord))}
-        if (currentDataType === 'face')  {fArr.push(Number(currentWord))}
-    } else { //if string is not numeric, then it is irrelvant until proven otherwise
-      currentDataType = 'irrelevant';
+      if (!(Number.isNaN(Number(currentWord)))) { //if string is numeric, then push it to approrpaite array depending of currentDataType
+        console.log(currentWord);
+        if (currentDataType === 'irrelevant') {}
+        if (currentDataType === 'vertex') {
+          vArr.push(Number(currentWord))
+        }
+        if (currentDataType === 'vertexNormal') {
+          vnArr.push(Number(currentWord))
+        }
+        if (currentDataType === 'face') {
+          fArr[faceIndex].push(Number(currentWord));
+          if (obj[i] === '\n') {
+            console.log('LINEBREAK')
+            faceIndex++;
+            fArr[faceIndex] = []
+          }
+        }
+      } else { //if string is not numeric, then it is irrelvant until proven otherwise
+        currentDataType = 'irrelevant';
+      }
+      /*//if at end of word and not current parsing a relevant datatype (v,vn,f)
+       then see if word indicates that this is a relevant datatype, otherwise it remains irrelevant*/
+      if (currentWord === 'v') {
+        currentDataType = 'vertex'
+      }
+      if (currentWord === 'vn') {
+        currentDataType = 'vertexNormal'
+      }
+      if (currentWord === 'f') {
+        currentDataType = 'face'
+      }
+      // console.log(currentWord);
+      currentWord = ''; //reset current word
+
+    } else { //if not a space
+      currentWord += obj[i]; //add current parsed character to currentWord
     }
-    /*//if at end of word and not current parsing a relevant datatype (v,vn,f)
-     then see if word indicates that this is a relevant datatype, otherwise it remains irrelevant*/
-    if (currentWord === 'v'){currentDataType = 'vertex'}
-    if (currentWord === 'vn'){currentDataType = 'vertexNormal'}
-    if (currentWord === 'f'){currentDataType = 'face'}
-    // console.log(currentWord);
-    currentWord = ''; //reset current word
-
-  } else { //if not a space
-    currentWord += obj[i]; //add current parsed character to currentWord
   }
-}
 
-//remove unnecessary values from face, leaving only the connected verts
-let reducedFArr = []
-for (let i = 0; i < fArr.length; i += 3){
-  reducedFArr.push(fArr[i])
-}
-fArr = reducedFArr;
+  //remove unnecessary values from face, leaving only the connected verts
 
-dLog(vArr);
-dLog(vnArr);
-dLog(fArr);
+  fArr.splice(fArr.length-1,1);
+  for (let i = 0; i < fArr.length; i++) {
+      let reducedFArr = [];
+    for (let j = 0; j < fArr[i].length; j += 3) {
+      reducedFArr.push(fArr[i][j]);
+    }
+      fArr[i] = reducedFArr;
+      reducedFArr = [];
+  }
+
+
+  dLog(vArr);
+  dLog(vnArr);
+  dLog(fArr);
 }
