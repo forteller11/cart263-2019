99a60710972ce2d32723807afe068177452fdb0b
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2019-03-28 16:38:37 -0400

    proj2 creating 3D vec class

--- projects/project3/js/helperFunctions/Vector3D.js
+++ projects/project3/js/helperFunctions/Vector3D.js
@@ -8,75 +8,94 @@ class Vector {
       this.y = a2; //y component of vector
       this.z = a3;
     }
-    this.mag = distFromDelta(this.x, this.y);
+    this.mag = distFromDelta(this.x, this.y, this.z);
   }
 
   mag() { //find the magnitude of this vetor
       console.log('undefined');
-      const newMag = distFromDelta(this.x, this.y);
+      const newMag = distFromDelta(this.x, this.y, this.z);
       this.mag = newMag;
       return newMag;
   }
 
   setMag(setMag) { //set mag and components to new mag
-      const thetaStore = Math.atan2(this.y, this.x); //save angle of vector
-      this.x = Math.cos(thetaStore) * setMag; //recalculate x component based on new mag
-      this.y = Math.sin(thetaStore) * setMag; //recalculate y component based on new mag
+      const thetaStore = Math.atan2(this.y, this.x, this.z); //save angle of vector
       this.mag=setMag;
+
+      const magDelta = this.mag - thetaStore;
+      this.x = this.x * magDelta; //recalculate x component based on new mag
+      this.y = this.y * magDelta; //recalculate y component based on new mag
+      this.z = this.z * magDelta; //recalculate z component based on new mag
   }
 
   sub(v){ //subtract a vector from this vector
     this.x -= v.x;
     this.y -= v.y;
-    this.mag = distFromDelta(this.x, this.y);
+    this.z -= v.z;
+    this.mag = distFromDelta(this.x, this.y, this.z);
   }
 
   add(v){ //add a vector from this vector
     this.x += v.x;
     this.y += v.y;
-    this.mag = distFromDelta(this.x, this.y);
+    this.z += v.z;
+    this.mag = distFromDelta(this.x, this.y, this.z);
   }
 
   div(divValue) { //divide vector by some magnitude
-    this.mag = distFromDelta(this.x, this.y);
-    const thetaStore = Math.atan2(this.y, this.x); //save angle of vector
-    this.mag = this.mag / divValue; //divide magnitude
-    this.x = Math.cos(thetaStore) * this.mag; //recalculate x component based on new mag
-    this.y = Math.sin(thetaStore) * this.mag; //recalculate y component based on new mag
+    this.mag /= divValue;
+    this.x   /= divValue;
+    this.y   /= divValue;
+    this.z   /= divValue;
   }
 
   mult(multValue) { //divide vector by some magnitude
-    this.mag = distFromDelta(this.x, this.y);
-    const thetaStore = Math.atan2(this.y, this.x); //save angle of vector
-    this.mag = this.mag * multValue; //divide magnitude
-    this.x = Math.cos(thetaStore) * this.mag; //recalculate x component based on new mag
-    this.y = Math.sin(thetaStore) * this.mag; //recalculate y component based on new mag
+    this.mag *= multValue;
+    this.x   *= multvalue;
+    this.y   *= multValue;
+    this.z   *= multValue;
   }
 
   constrainMag(max) { //make sure magnitude of vec isn't greator than "max"
-    this.mag = distFromDelta(this.x, this.y);
+    this.mag = distFromDelta(this.x, this.y, this.z);
 
     if (this.mag > max) { //if vector is larger than max
-      const thetaStore = Math.atan2(this.y, this.x); //save angle
+      toConstrainRatio = this.mag/max;
       this.mag = max; //set magnitude to max
-      this.x = Math.cos(thetaStore) * this.mag; //recalculate x component based on new mag
-      this.y = Math.sin(thetaStore) * this.mag; //recalculate y component based on new mag
+      this.x   *= toConstrainRatio;
+      this.y   *= toConstrainRatio;
+      this.z   *= toConstrainRatio;
     }
   }
 
   angle() { //returns angle in radians of current vector
+    console.log('angle return doesnt make sense in 3D')
     return Math.atan2(this.y, this.x); //save angle of vector
   }
 
-  rotate(rotateAmount) { //rotate vector by __ radians
-    this.mag = distFromDelta(this.x, this.y);
+  rotateZ(zAxis, yAxis, zAxis) { //rotate vector by __ radians
+    this.mag = distFromDelta(this.x, this.y, this.z);
     const thetaStore = Math.atan2(this.y, this.x); //save angle of vector
     this.x = Math.cos(thetaStore + rotateAmount) * this.mag;
     this.y = Math.sin(thetaStore + rotateAmount) * this.mag;
   }
 
+  rotateY(rotateAmount){
+    this.mag = distFromDelta(this.x, this.y, this.z);
+    const thetaStore = Math.atan2(this.z, this.x); //save angle of vector
+    this.x = Math.cos(thetaStore + rotateAmount) * this.mag;
+    this.z = Math.sin(thetaStore + rotateAmount) * this.mag;
+  }
+
+  rotateX(rotateAmount){
+    this.mag = distFromDelta(this.x, this.y, this.z);
+    const thetaStore = Math.atan2(this.z, this.y); //save angle of vector
+    this.y = Math.cos(thetaStore + rotateAmount) * this.mag;
+    this.z = Math.sin(thetaStore + rotateAmount) * this.mag;
+  }
+
   projectOnTo(otherVector){ //dot product
-    return (this.x*otherVector.x)+(this.y*otherVector.y);
+    return (this.x*otherVector.x)+(this.y*otherVector.y)+(this.z*otherVector.z);
   }
 
 }
