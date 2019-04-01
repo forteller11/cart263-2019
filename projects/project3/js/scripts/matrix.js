8e3d45ce97efa526748bf676a46656535a2474f2
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2019-04-01 13:11:14 -0400

    proj3 matrix mult debugging

--- projects/project3/js/scripts/matrix.js
+++ projects/project3/js/scripts/matrix.js
@@ -1,29 +1,15 @@
-matVecMult(arr2D,vec3D){ //matrix-Vector multiply
+function matVecMult(mat,vec){ //matrix-Vector multiply
+  //matrix is 2D array
+  //vector is 1D array
 
   let transformedVec = [];
-  for (let i = 0; i < vec3D.length; i ++){ //initialise arr at 0
+  for (let i = 0; i < mat.length; i ++){ //initialise vector output to have components === rows of matrix
     transformedVec[i] = 0;
   }
 
-  for (let i = 0; i < arr2D.length; i ++){ //rows of matrix
-    for (let j = 0; j < arr2D[i].length; j ++){ //collums of matrix
-      transformedVec[j] += arr2D[i][j] * vec3D[j]; //vec matrix mult
-    }
-  }
-
-  return transformedVec;
-}
-
-matVecDiv(arr2D,vec3D){ //matrix-Vector multiply
-
-  let transformedVec = [];
-  for (let i = 0; i < vec3D.length; i ++){ //initialise arr at 0
-    transformedVec[i] = 0;
-  }
-
-  for (let i = 0; i < arr2D.length; i ++){ //rows of matrix
-    for (let j = 0; j < arr2D[i].length; j ++){ //collums of matrix
-      transformedVec[j] += vec3D[j]/arr2D[i][j]; //vec matrix mult
+  for (let i = 0; i < mat.length; i ++){ //rows of matrix
+    for (let j = 0; j < mat[i].length; j ++){ //collums of matrix
+      transformedVec[i] += mat[i][j] * vec[j]; //vec matrix mult
     }
   }
 
