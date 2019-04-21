29bfe4dffb3bccbde526a6cb75ed3abcbe3c117d
Charly Yan Miller <fortelgaming@gmail.com>
2019-04-21 01:53:47 -0400

    proj3 added favicon

--- projects/project3/js/scripts/update.js
+++ projects/project3/js/scripts/update.js
@@ -1,6 +1,8 @@
 function update(){
-  //fill screen with yellow, update tick
+  //fill screen with yellow
   ctx.fillStyle = cssRGBA(bgColor);
   ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+
+  //update all systems
   systemManager.update();
 }
